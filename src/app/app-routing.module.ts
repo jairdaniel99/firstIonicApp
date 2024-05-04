09a6d26515fcd9c37c0678eae2cfd80accb292dd
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-05-04 11:02:52 -0600

    created pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'groceries',
+    loadChildren: () => import('./groceries/groceries.module').then( m => m.GroceriesPageModule)
+  },
+  {
+    path: 'electricity-bills',
+    loadChildren: () => import('./electricity-bills/electricity-bills.module').then( m => m.ElectricityBillsPageModule)
+  },
+  {
+    path: 'college-fees',
+    loadChildren: () => import('./college-fees/college-fees.module').then( m => m.CollegeFeesPageModule)
+  },
+  {
+    path: 'others',
+    loadChildren: () => import('./others/others.module').then( m => m.OthersPageModule)
   }
 ];
 @NgModule({
